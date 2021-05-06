c6ac5f0641ff04dbff62c7bf636c5cb08878b574
meet-ai <43718138+meet-ai@users.noreply.github.com>
2021-05-06 10:56:48 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'dd3a1929f66e4305a51e76c664d0c6c5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'dd3a1929f66e4305a51e76c664d0c6c5',
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '纵横山河万里',
+  domain: 'meetai.co',
+  author: 'kai',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '痴人说梦',
+  socialImageTitle: '痴人说梦',
+  socialImageSubtitle: '我有一个梦想! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  twitter: '',
+  github: 'meet-ai',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
